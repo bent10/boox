8c9e9f04d18590f03b9e59d0ecf8d3517a4dda3f
bent10 <bent10@stilearning.com>
2024-08-28 08:39:20 +0700

    fix: doogu

--- packages/cli/src/api.ts
+++ packages/cli/src/api.ts
@@ -59,74 +59,70 @@ export async function trainDataset(
   // Create Boox instance
   const boox = new Boox<Dataset>({ id, features, attributes, modelOptions })
 
-  try {
-    // Read dataset from file
-    const datasets = await oraPromise<Dataset[]>(
-      async () => JSON.parse(await readFile(resolvedSrc, 'utf8')),
-      {
-        text: 'Reading data...',
-        successText(data) {
-          return `Reading ${getDataSize(data)} data!`
-        }
+  // Read dataset from file
+  const datasets = await oraPromise<Dataset[]>(
+    async () => JSON.parse(await readFile(resolvedSrc, 'utf8')),
+    {
+      text: 'Reading data...',
+      successText(data) {
+        return `Reading ${getDataSize(data)} data!`
       }
-    )
-
-    const batches = await generateBatches(datasets)
-    const progress = {
-      current: 0,
-      length: datasets.length
     }
-    const startTime = new Date()
+  )
 
-    // Train the model in batches
-    await oraPromise(
-      ora => {
-        return Promise.all(
-          batches.map(batch =>
-            batch.map(dataset => {
-              progress.current++
-              ora.text = `Training ${resolvedSrc} ${progress.current} of ${progress.length} - ${getElapsedTime(startTime)}`
-              ora.render()
-              boox.addDocumentSync(dataset)
-            })
-          )
-        )
-      },
-      {
-        text: 'Start training...',
-        successText() {
-          return `Trained ${progress.current} documents in ${getElapsedTime(startTime)}`
-        }
-      }
-    )
-
-    // Compress and save the trained state
-    const compressor = isDeflate ? deflate : gzip
-    const state = JSON.stringify(boox.currentState)
-    const compressedState = compressor(state, {
-      level: DEFAULT_COMPRESSION_LEVEL
-    })
-
-    await oraPromise(
-      async () => {
-        const distDir = dirname(trainedFile)
-
-        try {
-          await access(distDir, constants.F_OK)
-        } catch {
-          await mkdir(distDir, { recursive: true })
-        }
-
-        return await writeFile(trainedFile, compressedState)
-      },
-      {
-        text: 'Saving...',
-        successText: `Saved ${getDataSize(state)} state to ${trainedFile}`
-      }
-    )
-  } catch (error) {
-    throw error
+  const batches = await generateBatches(datasets)
+  const progress = {
+    current: 0,
+    length: datasets.length
   }
+  const startTime = new Date()
+
+  // Train the model in batches
+  await oraPromise(
+    ora => {
+      return Promise.all(
+        batches.map(batch =>
+          batch.map(dataset => {
+            progress.current++
+            ora.text = `Training ${resolvedSrc} ${progress.current} of ${progress.length} - ${getElapsedTime(startTime)}`
+            ora.render()
+            boox.addDocumentSync(dataset)
+          })
+        )
+      )
+    },
+    {
+      text: 'Start training...',
+      successText() {
+        return `Trained ${progress.current} documents in ${getElapsedTime(startTime)}`
+      }
+    }
+  )
+
+  // Compress and save the trained state
+  const compressor = isDeflate ? deflate : gzip
+  const state = JSON.stringify(boox.currentState)
+  const compressedState = compressor(state, {
+    level: DEFAULT_COMPRESSION_LEVEL
+  })
+
+  await oraPromise(
+    async () => {
+      const distDir = dirname(trainedFile)
+
+      try {
+        await access(distDir, constants.F_OK)
+      } catch {
+        await mkdir(distDir, { recursive: true })
+      }
+
+      return await writeFile(trainedFile, compressedState)
+    },
+    {
+      text: 'Saving...',
+      successText: `Saved ${getDataSize(state)} state to ${trainedFile}`
+    }
+  )
 }
 
 /**
@@ -157,35 +153,28 @@ export async function searchDataset(
 
   const resolvedSrc = relative(process.cwd(), join(resolvedCwd, src))
 
-  try {
-    // Create Boox instance
-    const decompressor = isDeflate ? inflate : ungzip
+  // Create Boox instance
+  const decompressor = isDeflate ? inflate : ungzip
 
-    // Read trained state from file
-    console.time('Loading state')
-    const compressedState = await readFile(resolvedSrc)
-    const decompressedState = decompressor(compressedState, { to: 'string' })
-    const boox = new Boox({ modelOptions })
-    const state = JSON.parse(decompressedState)
-    // set state
-    boox.currentState = state
-    console.timeEnd('Loading state')
+  // Read trained state from file
+  console.time('Loading state')
+  const compressedState = await readFile(resolvedSrc)
+  const decompressedState = decompressor(compressedState, { to: 'string' })
+  const boox = new Boox({ modelOptions })
+  const state = JSON.parse(decompressedState)
+  // set state
+  boox.currentState = state
+  console.timeEnd('Loading state')
 
-    console.info('State size:', getDataSize(decompressedState))
+  console.info('State size:', getDataSize(decompressedState))
 
-    // Perform the search
-    // Load user config from (e.g. boox-results.config.js) file, if present
-    const resultsConfig: SearchOptions = await loadRc(
-      'boox-results',
-      resolvedCwd
-    )
-    console.time('Search in')
-    const results = await boox.search(query, resultsConfig)
-    console.timeEnd('Search in')
-    console.log()
+  // Perform the search
+  // Load user config from (e.g. boox-results.config.js) file, if present
+  const resultsConfig: SearchOptions = await loadRc('boox-results', resolvedCwd)
+  console.time('Search in')
+  const results = await boox.search(query, resultsConfig)
+  console.timeEnd('Search in')
+  console.log()
 
-    return Boox.paginateSearchResults(results, +offset, +length)
-  } catch (error) {
-    throw error
-  }
+  return Boox.paginateSearchResults(results, +offset, +length)
 }
--- packages/cli/test/index.test.ts
+++ packages/cli/test/index.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="vitest/globals" />
 
 import { readFile, unlink } from 'node:fs/promises'
